86653802aea22c2509389ca1ba694ce4c1827f40
songwon <songwon.bak@gmail.com>
2024-09-21 18:48:12 +0900

    finish chap 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "@/app/ui/global.css";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Acme Dashboard",
+  title: "%s | Acme Dashboard",
   description: "The official Next.js Course Dashboard, built with App Router.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
 };
